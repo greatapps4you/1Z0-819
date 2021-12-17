cdebb372bbb178af95d493af044ed8d8f990b57b
GreatApps4you <developer@greatapps4you.us>
2021-12-17 21:53:11 +0100

    [1Z0-819] myapp, a modularized app

---
+++ myapp-multimodule/myapp/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module abc.myapp {
+	requires datalayer;
+}
--- myapp/src/main/java/com/abc/myapp/Main.java
+++
@@ -1,8 +0,0 @@
-package com.abc.myapp;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Executing My App...");
-	}
-
-}
