ac4948454d6174cdfe28767864dd8b2cfd872b29
GreatApps4you <developer@greatapps4you.us>
2021-12-17 10:31:45 +0100

    [1Z0-819] CharAt

--- src/test/java/us/greatapps4you/ocp/mostmissed/MostMissedTest.java
+++ src/test/java/us/greatapps4you/ocp/mostmissed/MostMissedTest.java
@@ -1,11 +1,85 @@
 package us.greatapps4you.ocp.mostmissed;
 
+import java.security.AccessControlException;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 @TestInstance(Lifecycle.PER_CLASS)
 class MostMissedTest {
+	
+	@Test
+	void testConversion() {
+		char ch = 'A';
+		//Conversion from char to long does not need a cast.
+		long aLong = ch;
+		Assertions.assertEquals(65L, aLong);
+		
+		byte b = 65;
+		//Conversion from byte to short does not need a cast.
+		short s = b;
+		Assertions.assertEquals(65, s);
+		
+		//Conversion from short to char needs a cast.
+		char aUpperCase = (char)s;
+		Assertions.assertEquals('A', aUpperCase);
+		
+		int i = 10;
+		//Conversion from int to float does not need a cast.
+		float f = i;
+		Assertions.assertEquals(10, f);
+		
+		
+		byte cb = 65;
+		char cch = 'A';
+		short cs = 65;
+		
+		//Conversion from byte, char or short to int, long, or float does not need a cast.
+		int cInt = cb;
+		Assertions.assertTrue(cInt == cb);
+		cInt = cch;
+		Assertions.assertTrue(cInt == cch);
+		cInt = cs;
+		Assertions.assertTrue(cInt == cch);
+		
+		Assertions.assertTrue(cInt == cs);
+		
+		long caLong = cb;
+		Assertions.assertTrue(caLong == cb);
+		caLong = cch;
+		Assertions.assertTrue(caLong == cch);
+		caLong = cs;
+		Assertions.assertTrue(caLong == cs);
+		
+		float aFloat = cb;
+		Assertions.assertTrue(aFloat == cb);
+		aFloat = cch;
+		Assertions.assertTrue(aFloat == cch);
+		aFloat = cs;
+		Assertions.assertTrue(aFloat == cs);
+		
+	}
+	
+	
+	@Test
+	void testCharAt() {
+		String str = "12345";
+		char ch = str.charAt(3);
+		Assertions.assertEquals('4', ch);
+		
+		Assertions.assertThrows(StringIndexOutOfBoundsException.class, () -> {
+			char ch2 = str.charAt(str.length());
+			Assertions.assertEquals('3', ch2);
+		});
+		
+		Assertions.assertThrows(StringIndexOutOfBoundsException.class, () -> {
+			char ch2 = str.charAt(-1);
+			Assertions.assertEquals('3', ch2);
+		});
+		
+	}
 		
 	@Test
 	void testA() {
